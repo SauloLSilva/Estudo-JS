c7345a045b55d6a969b7a792a55058eb771eea8f
SauloLFSilva <ssilva@autodoc.com.br>
2023-10-31 23:36:48 -0300

    continuação de estudos em JS

---
+++ Aulas/JS14 - For.js
@@ -0,0 +1,13 @@
+// usando for
+// dividido em 3 inicialização, teste lógico e incremento
+// ex:
+// em while:
+// var c = 1
+// while (valor > 0){
+//     console.log(`${valor} está diminuindo`)
+//     valor -= 1
+// }
+// em for:
+for (var c=1;c<=10;c++){
+    console.log(c)
+}
